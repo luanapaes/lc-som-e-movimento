623edb9c339baed9a60b00be91fc0d8c71f7416e
Luana Paes <luanafranca598@gmail.com>
2025-05-06 13:24:16 -0300

    adiciona funcionalidade de carrossel com:
    - troca automática de imagens a cada 4 segundos
    - botões de navegação para slides anterior e próximo
    - transição suave entre as imagens usando opacidade

--- main.js
+++ main.js
@@ -2,3 +2,35 @@ function toggleMenu() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('active');
 }
+
+// carrossel
+const images = [
+    "/assets/carrossel/sem-2.jpeg",
+    "/assets/carrossel/sem-4.jpeg",
+    "/assets/carrossel/sem-5-1.jpeg",
+    "/assets/carrossel/sem-1.jpeg"
+];
+
+const imageElement = document.getElementById("carousel-image");
+let currentIndex = 0;
+
+function showImage(index) {
+    imageElement.style.opacity = 0;
+    setTimeout(() => {
+        imageElement.src = images[index];
+        imageElement.style.opacity = 1;
+    }, 300); 
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+}
+
+setInterval(nextSlide, 4000);
+
